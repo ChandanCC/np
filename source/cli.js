8124c89ed198a33c045b2d360de875e721377c93
Chandan Chhouda <chandan@geekyants.com>
2019-12-31 14:45:54 +0530

    Testing

--- source/cli.js
+++ source/cli.js
@@ -13,65 +13,65 @@ const util = require('./util');
 const ui = require('./ui');
 const np = require('.');
 
-const cli = meow(`
-	Usage
-	  $ np <version>
+// const cli = meow(`
+// 	Usage
+// 	  $ np <version>
 
-	  Version can be:
-	    ${version.SEMVER_INCREMENTS.join(' | ')} | 1.2.3
+// 	  Version can be:
+// 	    ${version.SEMVER_INCREMENTS.join(' | ')} | 1.2.3
 
-	Options
-	  --any-branch        Allow publishing from any branch
-	  --no-cleanup        Skips cleanup of node_modules
-	  --no-tests          Skips tests
-	  --yolo              Skips cleanup and testing
-	  --no-publish        Skips publishing
-	  --tag               Publish under a given dist-tag
-	  --no-yarn           Don't use Yarn
-	  --contents          Subdirectory to publish
-	  --no-release-draft  Skips opening a GitHub release draft
+// 	Options
+// 	  --any-branch        Allow publishing from any branch
+// 	  --no-cleanup        Skips cleanup of node_modules
+// 	  --no-tests          Skips tests
+// 	  --yolo              Skips cleanup and testing
+// 	  --no-publish        Skips publishing
+// 	  --tag               Publish under a given dist-tag
+// 	  --no-yarn           Don't use Yarn
+// 	  --contents          Subdirectory to publish
+// 	  --no-release-draft  Skips opening a GitHub release draft
 
-	Examples
-	  $ np
-	  $ np patch
-	  $ np 1.0.2
-	  $ np 1.0.2-beta.3 --tag=beta
-	  $ np 1.0.2-beta.3 --tag=beta --contents=dist
-`, {
-	booleanDefault: undefined,
-	flags: {
-		anyBranch: {
-			type: 'boolean'
-		},
-		cleanup: {
-			type: 'boolean'
-		},
-		tests: {
-			type: 'boolean'
-		},
-		yolo: {
-			type: 'boolean'
-		},
-		publish: {
-			type: 'boolean'
-		},
-		releaseDraft: {
-			type: 'boolean',
-			default: true
-		},
-		tag: {
-			type: 'string'
-		},
-		yarn: {
-			type: 'boolean'
-		},
-		contents: {
-			type: 'string'
-		}
-	}
-});
+// 	Examples
+// 	  $ np
+// 	  $ np patch
+// 	  $ np 1.0.2
+// 	  $ np 1.0.2-beta.3 --tag=beta
+// 	  $ np 1.0.2-beta.3 --tag=beta --contents=dist
+// `, {
+// 	booleanDefault: undefined,
+// 	flags: {
+// 		anyBranch: {
+// 			type: 'boolean'
+// 		},
+// 		cleanup: {
+// 			type: 'boolean'
+// 		},
+// 		tests: {
+// 			type: 'boolean'
+// 		},
+// 		yolo: {
+// 			type: 'boolean'
+// 		},
+// 		publish: {
+// 			type: 'boolean'
+// 		},
+// 		releaseDraft: {
+// 			type: 'boolean',
+// 			default: true
+// 		},
+// 		tag: {
+// 			type: 'string'
+// 		},
+// 		yarn: {
+// 			type: 'boolean'
+// 		},
+// 		contents: {
+// 			type: 'string'
+// 		}
+// 	}
+// });
 
-updateNotifier({pkg: cli.pkg}).notify();
+// updateNotifier({pkg: cli.pkg}).notify();
 
 (async () => {
 	const pkg = util.readPkg();
@@ -79,23 +79,21 @@ updateNotifier({pkg: cli.pkg}).notify();
 	const defaultFlags = {
 		cleanup: true,
 		tests: true,
-		publish: true,
+		publish: false,
 		yarn: hasYarn()
 	};
 
-	const localConfig = await config();
 
-	const flags = {
-		...defaultFlags,
-		...localConfig,
-		...cli.flags
-	};
+	const isAvailable = defaultFlags.publish
+		? await isPackageNameAvailable(pkg)
+		: false;
 
-	const isAvailable = flags.publish ? await isPackageNameAvailable(pkg) : false;
+	// const version = cli.input.length > 0 ? cli.input[0] : false;
 
-	const version = cli.input.length > 0 ? cli.input[0] : false;
-
-	const options = await ui({...flags, exists: !isAvailable, version}, pkg);
+	const options = await ui(
+		{ ...defaultFlags, exists: !isAvailable, version },
+		pkg
+	);
 
 	if (!options.confirm) {
 		process.exit(0);
